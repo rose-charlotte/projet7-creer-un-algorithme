3062bcd64e93f31d594388536488c19e0b744f4a
momilette <charlotte@anros.fr>
2023-11-03 12:27:21 +0100

    recipe + filter css

--- src/components/filters.ts
+++ src/components/filters.ts
@@ -1,9 +1,16 @@
 export function buildFilters(): HTMLElement {
     const body = document.querySelector("body");
     const filtersContainer = document.createElement("div");
-    const filter1 = document.createElement("select");
-    const filter2 = document.createElement("select");
-    const filter3 = document.createElement("select");
+    filtersContainer.className = "filters-container";
+    const filter1 = document.createElement("div");
+    filter1.className = "filter";
+    filter1.textContent = "Ingrédeients";
+    const filter2 = document.createElement("div");
+    filter2.className = "filter";
+    filter2.textContent = "Appareils";
+    const filter3 = document.createElement("div");
+    filter3.className = "filter";
+    filter3.textContent = "Ustensiles";
 
     body?.appendChild(filtersContainer);
     filtersContainer.appendChild(filter1);
