ce77d326a42839cf5b3dad4e2916228d41ea4d15
momilette <charlotte@anros.fr>
2023-11-07 12:46:52 +0100

    mise en place du filter

---
+++ src/components/toggleFilterModal.ts
@@ -0,0 +1,8 @@
+import { buildFilterModal } from "./buildFilterModal";
+
+export function toggleFilterModal() {
+    //const filterElement = document.querySelector<HTMLElement>(".filter");
+    console.log("je suis cliqué");
+    // const filterModal = document.querySelector()
+    buildFilterModal();
+}
