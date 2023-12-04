56c8788e805503539ee8854db787e4ec780cfd33
momilette <charlotte@anros.fr>
2023-12-04 11:08:09 +0100

    mis en place du filtre ustensils

--- src/filters/algorithmes/arrayMethods.ts
+++ src/filters/algorithmes/arrayMethods.ts
@@ -56,12 +56,21 @@ function recipeMatchAppliances(recipe: Recipe, selectedAppliances: Set<string>):
     if (selectedAppliances.size === 0) {
         return true;
     }
+    //console.log(selectedAppliances);
 }
 
 function recipeMatchUstensils(recipe: Recipe, selectedUstensils: Set<string>): boolean {
     if (selectedUstensils.size === 0) {
         return true;
     }
+    const recipeUstensils = new Set(recipe.ustensils);
+    console.log(recipeUstensils);
+    for (const ustensil of selectedUstensils) {
+        if (!recipeUstensils.has(ustensil)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 function recipeMatchGlobalSearch(recipe: Recipe, globalSearch: string | undefined): boolean {
