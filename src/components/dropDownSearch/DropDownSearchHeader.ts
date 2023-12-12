37d1dfe40e13b64ee00b0ba6c085ea5c21de1e9d
momilette <charlotte@anros.fr>
2023-12-12 18:48:24 +0100

    mise en place du click en dehors des dropDowwn

--- src/components/dropDownSearch/DropDownSearchHeader.ts
+++ src/components/dropDownSearch/DropDownSearchHeader.ts
@@ -1,8 +1,9 @@
+import { ComponentRender } from "../../ComponentRender";
 import styles from "./DropDownSearch.module.css";
 
 //Build the header element of the dropDown
 
-export function DropDownSearchHeader(props: DropDownSearchHeaderProps): HTMLElement {
+export function DropDownSearchHeader(props: DropDownSearchHeaderProps): ComponentRender<DropDownSearchHeaderProps> {
     const dropDownFilterContainer = document.createElement("div");
     dropDownFilterContainer.classList.add(styles.dropdownSearchHeader);
 
@@ -15,25 +16,44 @@ export function DropDownSearchHeader(props: DropDownSearchHeaderProps): HTMLElem
     arrowDown.src = "assets/icones/arrowDown.svg";
 
     dropDownFilterContainer.appendChild(arrowDown);
-
-    return dropDownFilterContainer;
+    return {
+        element: dropDownFilterContainer,
+        updateProps,
+    };
 
     function toggleOpenClose() {
-        const isOpened = dropDownFilterContainer.classList.toggle(styles.open);
+        props.onToggleOpenClose();
+    }
 
-        if (isOpened) {
-            arrowDown.src = "assets/icones/arrowUp.svg";
-            arrowDown.alt = "Fermer";
-        } else {
-            arrowDown.src = "assets/icones/arrowDown.svg";
-            arrowDown.alt = "Ouvrir";
+    function setClose() {
+        if (dropDownFilterContainer.classList.contains(styles.open)) {
+            dropDownFilterContainer.classList.remove(styles.open);
         }
 
-        props.onToggleOpenClose(false);
+        arrowDown.src = "assets/icones/arrowDown.svg";
+        arrowDown.alt = "Ouvrir";
+    }
+
+    function setOpen() {
+        if (!dropDownFilterContainer.classList.contains(styles.open)) {
+            dropDownFilterContainer.classList.add(styles.open);
+        }
+
+        arrowDown.src = "assets/icones/arrowUp.svg";
+        arrowDown.alt = "Fermer";
+    }
+
+    function updateProps(updatedProps: Partial<DropDownSearchHeaderProps>) {
+        if (updatedProps.isOpened === true) {
+            setOpen();
+        } else if (updatedProps.isOpened === false) {
+            setClose();
+        }
     }
 }
 
 export interface DropDownSearchHeaderProps {
     title: string;
-    onToggleOpenClose: (open: boolean) => void;
+    isOpened: boolean;
+    onToggleOpenClose: () => void;
 }
